33da21c6ab67036f7c11bd332e228d73073b68fc
Jacob Winfrey <jacob_winfrey@yahoo.com>
2023-05-24 13:41:06 -0600

    init

---
+++ planets.client/app/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-rs6rfcdz13xx8ylr.us.auth0.com'
+export const audience = 'http://sandbox.com'
+export const clientId = 'lsY26BZSOeavCi54t2CCuuahBK3dQ7Nj'
